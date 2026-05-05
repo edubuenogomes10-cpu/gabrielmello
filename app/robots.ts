cd4695a16b91c1bda68461bb2b6682b5a9824c22
gomesgbe <edubuenogomes10@gmail.com>
2026-05-05 09:01:25 -0300

    Fix static export for robots and sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = "https://www.gabrielmellofotografia.com.br";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = "https://www.gabrielmellofotografia.com.br";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
